e5c491e7ce9745c7f50316fbd91472e7e70655e7
Xavier <xaviermunroe@ac.c4q.nyc>
2019-02-01 15:32:38 -0500

    created component and graphql folders. Made first login component to test out react apollo again. I will begin building the login and register views next.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoginComponent from './components/auth/login'
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <LoginComponent />
       </div>
     );
   }
