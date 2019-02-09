afed541b0122e004ea72e6ba9da7c00d10ba2014
Xavier <xaviermunroe@ac.c4q.nyc>
2019-02-09 14:15:26 -0500

    imported config, confirmed verifyToken worked

--- backend/helpers/verifyToken.js
+++ backend/helpers/verifyToken.js
@@ -1,10 +1,15 @@
 import jsonwebtoken from 'jsonwebtoken'
+import config from '../config'
 
 export default (req, res, next) => {
     const tokenPresent = req.headers['authorization']
     if(tokenPresent){
         jsonwebtoken.verify(tokenPresent, config.jwtSecret, (err, valid) => {
+            if (err){
+                console.log('JSON TOKEN ERROR',err)
+            }
             if(valid){
+                console.log('VALID JSON TOKEN',valid)
                 req.user = valid
             }
         })
